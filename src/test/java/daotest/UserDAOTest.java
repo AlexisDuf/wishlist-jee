965dff28ddbe8655dd1e7521ac5b668e4852bd18
DUFOUR ALEXIS <alexis.dufour@decathlon.com>
2016-02-20 10:50:27 +0100

    some modif

--- src/test/java/daotest/UserDAOTest.java
+++ src/test/java/daotest/UserDAOTest.java
@@ -1,5 +1,6 @@
 package daotest;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import wishlist.dao.UserDAO;
@@ -7,24 +8,32 @@ import wishlist.model.User;
 import wishlist.service.ContainerHarness;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import javax.inject.Inject;
 import javax.naming.NamingException;
 
 
 public class UserDAOTest extends ContainerHarness{
 	
-	
+    @Inject
+    UserDAO dao;
+    
+    @Before
+    public void doBefore() throws NamingException {
+        dao = getEjb("UserDAO", UserDAO.class);
+    }
+    
     @Test
     public void itCanCreateAUser() throws Exception{    	
-		UserDAO dao = getUserDAO();
+
     	User testUser = dao.create("alexis.dufour@isen-lille.fr", "Alexis");
     	assertThat(testUser).isNotNull();
     	assertThat(testUser.getMail()).isEqualTo("alexis.dufour@isen-lille.fr");
     	assertThat(testUser.getName()).isEqualTo("Alexis");	    			
     }
     
-    private UserDAO getUserDAO() throws NamingException {
+    /*private UserDAO getUserDAO() throws NamingException {
         final UserDAO udao = (UserDAO) container.getContext().lookup(
                 "java:global/wishlist-jee/UserDAO");
         return udao;
-    }
+    }*/
 }
