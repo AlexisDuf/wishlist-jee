dcf50de14160b5e5ccf82027c113c70d8b94bda1
Matthieu DELMAIRE <matt@macbook-pro-de-matthieu.home>
2016-02-23 17:41:29 +0100

    feat(api) : Seconde version API, compatibility with JSON

--- rest-example/src/main/java/org/superbiz/rest/service/AuthRestService.java
+++ rest-example/src/main/java/org/superbiz/rest/service/AuthRestService.java
@@ -4,70 +4,57 @@ import java.util.List;
 
 import javax.ejb.EJB;
 import javax.inject.Inject;
+import javax.json.Json;
 import javax.transaction.HeuristicMixedException;
 import javax.transaction.HeuristicRollbackException;
 import javax.transaction.NotSupportedException;
 import javax.transaction.RollbackException;
 import javax.transaction.SystemException;
 import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.superbiz.rest.dao.UserDAO;
 import org.superbiz.rest.model.User;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 
 
 
 @Path("/api/users")
-@Produces({"text/xml"})
 public class AuthRestService {
 	
 	@EJB
 	private UserDAO udao;
 	
+	@POST
 	@Path("/create")
-	@PUT
-    public String create(@QueryParam("mail") String mail, @QueryParam("name") String name) throws SecurityException, IllegalStateException, NotSupportedException, SystemException, RollbackException, HeuristicMixedException, HeuristicRollbackException {
+    public Response create(@FormParam("mail") String mail, @FormParam("name") String name) throws SecurityException, IllegalStateException, NotSupportedException, SystemException, RollbackException, HeuristicMixedException, HeuristicRollbackException {
 		User user = udao.findByMail(mail);
 		if(user == null){
 			user = udao.create(mail, name);
 		}
-		JsonObject usr = new JsonObject();
-		usr.addProperty("id", user.getId());
-		usr.addProperty("mail", user.getMail());
-		usr.addProperty("name", user.getName());
-		return usr.toString();
+		Gson gson = new Gson();
+		return Response.ok(gson.toJson(user), MediaType.APPLICATION_JSON).build();
     }
 	
     
-    
+	@GET
     @Path("/list")
-    @GET
-    public List<User> list(@QueryParam("first") @DefaultValue("0") int first,
-                           @QueryParam("max") @DefaultValue("20") int max) {
-        return udao.list(first, max);
+    public Response list(@FormParam("first") @DefaultValue("0") int first,
+    						@FormParam("max") @DefaultValue("20") int max) {
+        List<User> users = udao.list(first, max);
+        Gson gson = new Gson();
+        return Response.ok(gson.toJson(users), MediaType.APPLICATION_JSON).build();
     }
-    
-    
-	
-/*	@POST
-    public User create(User usr) throws SecurityException, IllegalStateException, NotSupportedException, SystemException, RollbackException, HeuristicMixedException, HeuristicRollbackException {
-		User user = udao.findByMail(usr.getMail());
-
-		if(user == null){
-			user = udao.create(usr.getMail(), usr.getName());
-		}
-		return user;
-    }*/
-	
-
 	
 }
--- rest-example/src/main/java/org/superbiz/rest/service/WishListRestService.java
+++ rest-example/src/main/java/org/superbiz/rest/service/WishListRestService.java
@@ -7,11 +7,14 @@ import javax.ejb.EJB;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.superbiz.rest.dao.CommentDAO;
@@ -33,7 +36,6 @@ import javax.ws.rs.QueryParam;
 
 
 @Path("/api/wishlists")
-@Produces({"text/xml"})
 public class WishListRestService{
 	
 	/*
@@ -98,48 +100,30 @@ public class WishListRestService{
 	/*
 	 * Create a new wishlist
 	 */
+	@POST
 	@Path("/create")
-	@PUT
-    public String createWishList(@QueryParam("description") String description,
-            @QueryParam("user_id") int user_id) {
-		Wishlist w = wdao.create(description, user_id);		
-		w = wdao.find(w.getId());
-		JsonObject wishlistJson = new JsonObject();
-		wishlistJson.addProperty("id", w.getId());
-		wishlistJson.addProperty("description", w.getDescription());
-		wishlistJson.addProperty("tokenAdmin", w.getTokenAdmin());
-		wishlistJson.addProperty("tokenGuest", w.getTokenGuest());
-		wishlistJson.addProperty("creator", w.getCreator().getName());
-        return wishlistJson.toString();
+    public Response createWishList(@FormParam("description") String description,
+            @FormParam("user_id") int user_id) {
+		Wishlist wishList = wdao.create(description, user_id);		
+		Gson gson = new Gson();
+        return Response.ok(gson.toJson(wishList), MediaType.APPLICATION_JSON).build();
     }
 	
 	
 	/*
 	 * Get a particular wishlist by token
 	 */
+	@GET
     @Path("/{wishlist_token}")
-    @GET
     public Response showParticularWishList(@PathParam("wishlist_token") String token)  {
-    	JsonObject responseObj = new JsonObject();
     	Gson gson = new Gson();
     	if(isAdmin(token)){
     		Wishlist wishlist = wdao.loadFromTokenAdmin(token);
-    		String itemsObj = this.parseAdminItems(wishlist);
-			responseObj.addProperty("items", itemsObj);
-			responseObj.addProperty("guests", gson.toJson(wishlist.getGuest()));
-			responseObj.addProperty("admin", gson.toJson(wishlist.getCreator()));
-			responseObj.addProperty("created_date", gson.toJson(wishlist.getCreated()));
-			responseObj.addProperty("description", gson.toJson(wishlist.getDescription()));
-			return Response.status(Response.Status.OK).build();
+    		return Response.ok(gson.toJson(wishlist), MediaType.APPLICATION_JSON).build();
     	}
     	else if(isGuest(token)){
     		Wishlist wishlist = wdao.loadFromTokenGuest(token);
-			responseObj.addProperty("items", gson.toJson(wishlist.getItem()));
-			responseObj.addProperty("guests", gson.toJson(wishlist.getGuest()));
-			responseObj.addProperty("admin", gson.toJson(wishlist.getCreator()));
-			responseObj.addProperty("created_date", gson.toJson(wishlist.getCreated()));
-			responseObj.addProperty("description", gson.toJson(wishlist.getDescription()));
-			return Response.status(Response.Status.OK).build();
+    		return Response.ok(gson.toJson(wishlist), MediaType.APPLICATION_JSON).build();
     	}
     	else{
     		return Response.status(Response.Status.FORBIDDEN).build();
@@ -150,15 +134,15 @@ public class WishListRestService{
     /*
      * Modify a particular wishlist, only admin can do that
      */
+	@POST
     @Path("/{wishlist_token}")
-    @POST
-    public Response updateWishList(@PathParam("wishlist_token") String token, @QueryParam("description") String description){
+    public Response updateWishList(@PathParam("wishlist_token") String token, @FormParam("description") String description){
     	// Admin can modify wishlist configurations
     	if(this.isAdmin(token)){
     		Wishlist wishlist = wdao.loadFromTokenAdmin(token);
     		Gson gson = new Gson();
     		wishlist = wdao.updateDescription(wishlist.getId(), description);
-    		return Response.status(Response.Status.OK).entity(gson.toJson(wishlist)).build();
+    		return Response.ok(gson.toJson(wishlist), MediaType.APPLICATION_JSON).build();
     	}
     	// Guets can't modify wishlist configurations
     	else{
@@ -170,8 +154,8 @@ public class WishListRestService{
     /*
      * Delete a particular wishlist, only admin can do that
      */
+	@DELETE
     @Path("/{wishlist_token}")
-    @DELETE
     public Response deleteWishList(@PathParam("wishlist_token") String token){
     	// Admin can delete wishlist
     	if(this.isAdmin(token)){
@@ -189,10 +173,10 @@ public class WishListRestService{
     /*
      * Add item
      */
+	@POST
     @Path("{wishlist_token}/items")
-    @POST
-    public Response createItem(@PathParam("wishlist_token") String token, @QueryParam("avg_price") float avg_price,
-            @QueryParam("url_photo") String url_photo){
+    public Response createItem(@PathParam("wishlist_token") String token, @FormParam("avg_price") float avg_price,
+            @FormParam("url_photo") String url_photo){
     	Wishlist wishlist = wdao.loadFromTokenAdmin(token);
     	// Guests can't add item 
     	if(wishlist == null){
@@ -202,7 +186,7 @@ public class WishListRestService{
     	else{
     		Gson gson = new Gson();
     		WishlistItem item = witemdao.create(avg_price, url_photo, wishlist.getId());
-        	return Response.status(Response.Status.OK).entity(gson.toJson(item)).build();
+    		return Response.ok(gson.toJson(item), MediaType.APPLICATION_JSON).build();
     	}
     }
     
@@ -210,10 +194,10 @@ public class WishListRestService{
     /*
      * Modify a specific item
      */
+	@PUT
     @Path("{wishlist_token}/items/{item_id}")
-    @PUT
-    public Response updateItem(@PathParam("wishlist_token") String token, @PathParam("item_id") long item_id, @QueryParam("avg_price") float avg_price,
-            @QueryParam("url_photo") String url_photo){
+    public Response updateItem(@PathParam("wishlist_token") String token, @PathParam("item_id") long item_id, @FormParam("avg_price") float avg_price,
+            @FormParam("url_photo") String url_photo){
     	Wishlist wishlist = wdao.loadFromTokenAdmin(token);
     	// Guests can't update item 
     	if(wishlist == null){
@@ -231,8 +215,8 @@ public class WishListRestService{
     /*
      * Delete a specific item 
      */
+	@DELETE
     @Path("{wishlist_token}/items/{item_id}")
-    @DELETE
     public Response deleteItem(@PathParam("wishlist_token") String token, @PathParam("item_id") long item_id){
     	Wishlist wishlist = wdao.loadFromTokenAdmin(token);
     	// Guests can't delete item 
@@ -250,15 +234,15 @@ public class WishListRestService{
     /*
      * Add comment
      */
-    @Path("{wishlist_token}/items/{item_id}/comments")
     @POST
-    public Response createComment(@PathParam("wishlist_token") String token, @PathParam("item_id") long item_id,@QueryParam("content") String content,
-            @QueryParam("user_id") long user_id){
+    @Path("{wishlist_token}/items/{item_id}/comments")
+    public Response createComment(@PathParam("wishlist_token") String token, @PathParam("item_id") long item_id,@FormParam("content") String content,
+            @HeaderParam("user_id") long user_id){
     	// Guest can add comment
     	if(this.isGuest(token)){
     		Gson gson = new Gson();
     		Comment comment = cmtdao.create(content, item_id, user_id);
-    		return Response.status(Response.Status.OK).entity(gson.toJson(comment)).build();
+    		return Response.ok(gson.toJson(comment), MediaType.APPLICATION_JSON).build();
     	}
     	// Admin can't add comment
     	else{
@@ -269,17 +253,17 @@ public class WishListRestService{
     /*
      * Modify a particular comment
      */
-    @Path("{wishlist_token}/items/{item_id}/comments/{comment_id}")
     @PUT
-    public Response createComment(@PathParam("wishlist_token") String token, @PathParam("item_id") long item_id, @PathParam("comment_id") long comment_id, @QueryParam("content") String content,
-            @QueryParam("user_id") long user_id){
+    @Path("{wishlist_token}/items/{item_id}/comments/{comment_id}")
+    public Response createComment(@PathParam("wishlist_token") String token, @PathParam("item_id") long item_id, @PathParam("comment_id") long comment_id, @FormParam("content") String content,
+            @HeaderParam("user_id") long user_id){
     	// Guest can modify comment only if is owner
     	if(this.isGuest(token)){
     		Comment comment = cmtdao.find(comment_id);
     		if(user_id == comment.getAuthor().getId()){
     			Gson gson = new Gson();
         		comment = cmtdao.update(comment_id, content);
-        		return Response.status(Response.Status.OK).entity(gson.toJson(comment)).build();
+        		return Response.ok(gson.toJson(comment), MediaType.APPLICATION_JSON).build();
     		}
     		else{
     			return Response.status(Response.Status.FORBIDDEN).build();
@@ -295,9 +279,9 @@ public class WishListRestService{
     /*
      * Delete a particular comment
      */
-    @Path("{wishlist_token}/items/{item_id}/comments/{comment_id}")
     @DELETE
-    public Response deleteComment(@PathParam("wishlist_token") String token, @PathParam("item_id") long item_id, @PathParam("comment_id") long comment_id, @QueryParam("user_id") long user_id){
+    @Path("{wishlist_token}/items/{item_id}/comments/{comment_id}")
+    public Response deleteComment(@PathParam("wishlist_token") String token, @PathParam("item_id") long item_id, @PathParam("comment_id") long comment_id, @HeaderParam("user_id") long user_id){
     	// Guest can delete comment only if is owner
     	if(this.isGuest(token)){
     		Comment comment = cmtdao.find(comment_id);
@@ -319,15 +303,15 @@ public class WishListRestService{
     /*
      * Add proposition
      */
-    @Path("{wishlist_token}/items/{item_id}/propositions")
     @POST
-    public Response createProposition(@PathParam("wishlist_token") String token, @PathParam("item_id") long item_id, @QueryParam("price") float price,
-            @QueryParam("user_id") long user_id){
+    @Path("{wishlist_token}/items/{item_id}/propositions")
+    public Response createProposition(@PathParam("wishlist_token") String token, @PathParam("item_id") long item_id, @FormParam("price") float price,
+            @HeaderParam("user_id") long user_id){
     	// Guest can add proposition
     	if(this.isGuest(token)){
     		Gson gson = new Gson();
     		GuestProposition proposition = gpdao.create(price, item_id, user_id);
-    		return Response.status(Response.Status.OK).entity(gson.toJson(proposition)).build();
+    		return Response.ok(gson.toJson(proposition), MediaType.APPLICATION_JSON).build();
     	}
     	// Admin can't add proposition
     	else{
@@ -339,17 +323,17 @@ public class WishListRestService{
     /*
      * Modify proposition
      */
-    @Path("{wishlist_token}/items/{item_id}/propositions/{proposition_id}")
     @PUT
-    public Response updateProposition(@PathParam("wishlist_token") String token,@PathParam("item_id") long item_id, @PathParam("proposition_id") long proposition_id, @QueryParam("price") float price,
-            @QueryParam("user_id") long user_id){
+    @Path("{wishlist_token}/items/{item_id}/propositions/{proposition_id}")
+    public Response updateProposition(@PathParam("wishlist_token") String token,@PathParam("item_id") long item_id, @PathParam("proposition_id") long proposition_id, @FormParam("price") float price,
+            @HeaderParam("user_id") long user_id){
     	// Guest can update proposition only if is owner
     	if(this.isGuest(token)){
     		GuestProposition proposition = gpdao.find(proposition_id);
     		if(user_id == proposition.getGuestName().getId()){
     			Gson gson = new Gson();
     			proposition = gpdao.update(proposition_id, price);
-    			return Response.status(Response.Status.OK).entity(gson.toJson(proposition)).build();
+    			return Response.ok(gson.toJson(proposition), MediaType.APPLICATION_JSON).build();
     		}
     		else{
     			return Response.status(Response.Status.FORBIDDEN).build();
@@ -368,7 +352,7 @@ public class WishListRestService{
     @Path("{wishlist_token}/items/{item_id}/propositions/{proposition_id}")
     @DELETE
     public Response deleteProposition(@PathParam("wishlist_token") String token,@PathParam("item_id") long item_id, @PathParam("proposition_id") long proposition_id,
-            @QueryParam("user_id") long user_id){
+            @HeaderParam("user_id") long user_id){
     	// Guest can delete proposition only if is owner
     	if(this.isGuest(token)){
     		GuestProposition proposition = gpdao.find(proposition_id);
@@ -391,9 +375,9 @@ public class WishListRestService{
     /*
      * Add item's link
      */
-    @Path("{wishlist_token}/items/{item_id}/links")
     @POST
-    public Response createLink(@PathParam("wishlist_token") String token, @PathParam("item_id") long item_id,@QueryParam("url") String url){
+    @Path("{wishlist_token}/items/{item_id}/links")
+    public Response createLink(@PathParam("wishlist_token") String token, @PathParam("item_id") long item_id,@FormParam("url") String url){
     	// Guest can't add link
     	if(this.isGuest(token)){
     		return Response.status(Response.Status.FORBIDDEN).build();
@@ -402,7 +386,7 @@ public class WishListRestService{
     	else{
     		Gson gson = new Gson();
     		Link link = ldao.create(url, item_id);
-    		return Response.status(Response.Status.OK).entity(gson.toJson(link)).build();
+    		return Response.ok(gson.toJson(link), MediaType.APPLICATION_JSON).build();
     	}
     }
     
@@ -410,8 +394,8 @@ public class WishListRestService{
     /*
      * Remove item's link
      */
-    @Path("{wishlist_token}/items/{item_id}/links/{link_id}")
     @DELETE
+    @Path("{wishlist_token}/items/{item_id}/links/{link_id}")
     public Response createLink(@PathParam("wishlist_token") String token, @PathParam("item_id") long item_id, @PathParam("link_id") long link_id){
     	// Guest can't remove link
     	if(this.isGuest(token)){
@@ -423,45 +407,4 @@ public class WishListRestService{
     		return Response.status(Response.Status.NO_CONTENT).build();
     	}
     }
-
-    
-    /*
-     * Add guest 
-     */
-    @Path("{wishlist_token}/guests/")
-    @POST
-    public Response addGuest(@PathParam("wishlist_token") String token, @QueryParam("email") String email){
-    	// Guest can't add guest
-    	if(this.isGuest(token)){
-    		return Response.status(Response.Status.FORBIDDEN).build();
-    	}
-    	// Admin can add guest
-    	else{
-    		/*
-    		 * TODO model
-    		 */
-    		return Response.status(Response.Status.OK).build();
-    	}
-    }
-    
-    
-    /*
-     * Remove guest
-     */
-    @Path("{wishlist_token}/guests/{user_id}")
-    @POST
-    public Response removeGuest(@PathParam("wishlist_token") String token, @PathParam("user_id") long user_id, @QueryParam("email") String email){
-    	// Guest can't add guest
-    	if(this.isGuest(token)){
-    		return Response.status(Response.Status.FORBIDDEN).build();
-    	}
-    	// Admin can add guest
-    	else{
-    		/*
-    		 * TODO model
-    		 */
-    		return Response.status(Response.Status.OK).build();
-    	}
-    }
-
 }
